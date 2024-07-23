49b8c6ac321765561516751ad38f338794601bdd
Juan A <j.alejandrominaya@gmail.com>
2024-07-23 15:07:15 -0600

    feat: Create mock data store

--- src/core/ports.rs
+++ src/core/ports.rs
@@ -2,7 +2,7 @@ use crate::core::tasks::*;
 use std::vec::IntoIter;
 
 pub trait DataStore {
-    fn write(&self, task_itr: IntoIter<Box<Task>>) -> bool;
+    fn write(&self, task_itr: IntoIter<&Task>) -> bool;
     fn write_task(&self, task: &Task) -> bool;
     fn read(&self) -> IntoIter<Box<Task>>;
     fn read_task(&self, id: u32) -> Box<Task>;
@@ -16,41 +16,22 @@ pub mod test {
     pub struct MockDataStore {
         write_return_val: bool,
         write_task_return_val: bool,
-        read_return_val: bool,
-        reead_task_return_val: bool,
     }
 
     impl DataStore for MockDataStore {
-        fn write(&self, _task_itr: IntoIter<Box<Task>>) -> bool {
+        fn write(&self, _task_itr: IntoIter<&Task>) -> bool {
             self.write_return_val
         }
         fn write_task(&self, _task: &Task) -> bool {
             self.write_task_return_val
         }
         fn read(&self) -> IntoIter<Box<Task>> {
-            let mut task_a = Box::new(Task::default());
-            let task_b = Box::new(Task {
-                id: 1,
-                ..Default::default()
-            });
-            let mut task_c = Box::new(Task {
-                id: 2,
-                ..Default::default()
-            });
-
-            let subtask_a = Box::new(Task {
-                id: 3,
-                ..Default::default()
-            });
-            let subtask_b = Box::new(Task {
-                id: 4,
-                ..Default::default()
-            });
-            let subtask_c = Box::new(Task {
-                id: 5,
-                ..Default::default()
-            });
-
+            let mut task_a = Box::new(Task::new_with_id(0));
+            let task_b = Box::new(Task::new_with_id(1));
+            let mut task_c = Box::new(Task::new_with_id(2));
+            let subtask_a = Box::new(Task::new_with_id(3));
+            let subtask_b = Box::new(Task::new_with_id(4));
+            let subtask_c = Box::new(Task::new_with_id(5));
             task_a.add_subtask(subtask_a);
 
             task_c.add_subtask(subtask_b);
@@ -60,11 +41,7 @@ pub mod test {
             test_tasks.into_iter()
         }
         fn read_task(&self, id: u32) -> Box<Task> {
-            let task = Task {
-                id,
-                ..Default::default()
-            };
-            Box::new(task)
+            Box::new(Task::new_with_id(id))
         }
     }
 }
