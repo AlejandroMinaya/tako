99499599c4db4ee43140c54a16f2ea9f921c379f
Juan A <j.alejandrominaya@gmail.com>
2024-07-23 16:05:55 -0600

    refa: Remove unnecessary methods (for now)

--- src/core/ports.rs
+++ src/core/ports.rs
@@ -4,9 +4,7 @@ use std::vec::IntoIter;
 
 pub trait DataStore: Debug {
     fn write(&self, task_itr: IntoIter<&Task>) -> bool;
-    fn write_task(&self, task: &Task) -> bool;
     fn read(&self) -> IntoIter<Box<Task>>;
-    fn read_task(&self, id: u32) -> Box<Task>;
 }
 
 #[cfg(test)]
@@ -16,15 +14,11 @@ pub mod test {
     #[derive(Debug, Default)]
     pub struct MockDataStore {
         write_return_val: bool,
-        write_task_return_val: bool,
     }
 
     impl DataStore for MockDataStore {
         fn write(&self, _task_itr: IntoIter<&Task>) -> bool {
             self.write_return_val
-        }
-        fn write_task(&self, _task: &Task) -> bool {
-            self.write_task_return_val
         }
         fn read(&self) -> IntoIter<Box<Task>> {
             let mut task_a = Box::new(Task::new_with_id(0));
@@ -40,9 +34,6 @@ pub mod test {
 
             let test_tasks = vec![task_a, task_b, task_c];
             test_tasks.into_iter()
-        }
-        fn read_task(&self, id: u32) -> Box<Task> {
-            Box::new(Task::new_with_id(id))
         }
     }
 }
