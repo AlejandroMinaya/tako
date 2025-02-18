0007634dba7f6fd44b0b421b3c6f43447f96795e
Juan A <j.alejandrominaya@gmail.com>
2025-02-18 18:46:51 +0100

    chore!: Dependencies initial cleanup

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -1,4 +1,2 @@
-#[cfg(feature = "rest_api")]
-pub mod api;
 #[cfg(feature = "wasm_app")]
 pub mod wasm_app;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ mod app;
 mod infra;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let oswald = Oswald::default();
-    infra::wasm_app::start(oswald).await.unwrap();
+
+    #[cfg(feature = "wasm_app")]
+    if cfg!(feature = "wasm_app") {
+        infra::wasm_app::start(oswald).await.unwrap();
+    }
+
+    #[cfg(feature = "rpc")]
+    if cfg!(feature = "rpc") {
+        println!("rpc not implemented yet");
+    }
+
+    Ok(())
 }
